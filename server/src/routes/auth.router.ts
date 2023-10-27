5a4d7d35f62c07171c06a64e04218c7ec645286a
Aleksandr <naberezhnyy.an@gmail.com>
2023-10-27 15:06:30 +0200

    fix creating router with correct type

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import AuthController from "../controllers/auth.controller.js";
-const router = new (Router as any)();
+const router = Router();
 
 router.post("/login", AuthController.login);
 router.post("/registration", AuthController.registration);

1b008bb78e37dafa7b54a45e6f98159bc20291b7
Aleksandr <naberezhnyy.an@gmail.com>
2023-10-31 19:32:42 +0100

    add class for db queries

--- server/src/database/queries/admin.queries.ts
+++ server/src/database/queries/admin.queries.ts
@@ -7,66 +7,57 @@ import {
 } from "../../utils/db.utils.js";
 import pool from "../index.js";
 
-const db = {
-  getHashedPassword: async function (name: string) {
+class DB {
+  async getHashedPassword(name: string) {
     const response: QueryResult<HashedPassword> = await pool.query(
       "SELECT password FROM users WHERE username = $1",
       [name]
     );
 
     return response.rows;
-  },
-  findUserByName: async function (name: string) {
+  }
+  async findUserByName(name: string) {
     const response: QueryResult<User> = await pool.query(
       "SELECT * FROM users WHERE username = $1",
       [name]
     );
     return response.rows;
-  },
-  findUserByID: async function (id: string) {
+  }
+  async findUserByID(id: string) {
     const response: QueryResult<User> = await pool.query(
       "SELECT * FROM users WHERE id = $1",
       [id]
     );
     return response.rows;
-  },
-  createUser: async function (
-    name: string,
-    email: string,
-    password: string,
-    role: role
-  ) {
+  }
+  async createUser(name: string, email: string, password: string, role: role) {
     const response: QueryResult<UserID> = await pool.query(
       "INSERT INTO users(username, email,password, role) VALUES ($1,$2,$3,$4) RETURNING id",
       [name, email, password, role]
     );
     return response.rows;
-  },
-  getUsers: async function () {
+  }
+  async getUsers() {
     const response: QueryResult<User> = await pool.query(
       "select users.id, users.username,users.email,users.password, users.role,TO_CHAR(created_at, 'HH24:MI:SS  DD.MM.YYYY') AS created_at from users ORDER BY users.id ASC"
     );
     return response.rows;
-  },
-  editUser: async function (
+  }
+  async editUser(
     id: string,
     name: string | undefined,
     email: string | undefined
   ) {
     const response = await pool.query(generateEditingQuery(id, name, email));
-  },
-  addAction: async function (
-    id: string | number,
-    action: Action,
-    params?: PropsToEdit
-  ) {
-    const response:QueryResult<ActionID> = await pool.query(
+  }
+  async addAction(id: string | number, action: Action, params?: PropsToEdit) {
+    const response: QueryResult<ActionID> = await pool.query(
       "INSERT INTO users_actions (user_id, action_type, action_data) VALUES ($1,$2,$3) RETURNING id",
       [id, action, generateActionMessage(action, params)]
     );
 
-    return response.rows
-  },
-};
+    return response.rows;
+  }
+}
 
-export default db;
+export default new DB();

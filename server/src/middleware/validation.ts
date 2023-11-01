5296a2afab57a9decab14f78ace49e3f96f26a69
Aleksandr <naberezhnyy.an@gmail.com>
2023-11-01 12:17:18 +0100

    restucture validators rules

--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -1,32 +1,49 @@
 import { body, oneOf } from "express-validator";
 
 export const validateRegistration = [
-  body("username", "Username is required.").notEmpty(),
-  body("password", "Password is required.").notEmpty(),
-  body("email", "Email is required.").notEmpty(),
-  body("username", "Username must be between 4 and 15 characters").isLength({
-    min: 4,
-    max: 15,
-  }),
-  body("password", "Password must be between 4 and 15 characters").isLength({
-    min: 4,
-    max: 15,
-  }),
-  body("email")
+  body("username", "Username is required.")
+    .notEmpty()
+    .isLength({
+      min: 4,
+      max: 15,
+    })
+    .withMessage("Username must be between 4 and 15 characters"),
+
+  body("password", "Password is required.")
+    .notEmpty()
+    .isLength({
+      min: 4,
+      max: 15,
+    })
+    .withMessage("Password must be between 4 and 15 characters"),
+
+  body("email", "Email is required.")
+    .notEmpty()
+    .isLength({
+      min: 4,
+      max: 15,
+    })
+    .withMessage("Email must be between 4 and 15 characters")
     .isEmail()
     .withMessage("Invalid email format"),
 ];
+
 export const validateLogin = [
-  body("username", "Username is required.").notEmpty(),
-  body("password", "Password is required.").notEmpty(),
-  body("username", "Username must be between 4 and 15 characters").isLength({
-    min: 4,
-    max: 15,
-  }),
-  body("password", "Password must be between 4 and 15 characters").isLength({
-    min: 4,
-    max: 15,
-  }),
+  body("username", "Username is required.")
+    .notEmpty()
+    .isLength({
+      min: 4,
+      max: 15,
+    })
+    .withMessage("Username must be between 4 and 15 characters"),
+
+  body("password", "Password is required.")
+    .notEmpty()
+    .isLength({
+      min: 4,
+      max: 15,
+    })
+    .withMessage("Password must be between 4 and 15 characters"),
 ];
 
 export const validateEdititng = [
@@ -36,9 +53,11 @@ export const validateEdititng = [
   oneOf([body("username").notEmpty(), body("email").notEmpty()], {
     message: "Username or email are required",
   }),
+
   body("email")
     .if(body("email").notEmpty())
     .isEmail()
+    .isLength({ min: 4, max: 15 })
     .withMessage("Invalid email format"),
   body("username")
     .if(body("username").notEmpty())

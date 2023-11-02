7c5f85840740d46443714ba3a19635478ba624b0
Aleksandr <naberezhnyy.an@gmail.com>
2023-11-02 16:51:03 +0100

    update schema

--- server/prisma/migrations/0_init/migration.sql
+++ server/prisma/migrations/0_init/migration.sql
@@ -1,10 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('user', 'admin');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
-    "role" VARCHAR(20) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
     "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
@@ -13,13 +16,19 @@ CREATE TABLE "users" (
 -- CreateTable
 CREATE TABLE "users_actions" (
     "id" SERIAL NOT NULL,
-    "user_id" INTEGER,
+    "user_id" INTEGER NOT NULL,
     "action_type" VARCHAR(15) NOT NULL,
-    "action_data" JSONB,
+    "action_data" JSONB NOT NULL,
     "action_time" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_actions_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_email_key" ON "users"("id", "email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_actions_id_key" ON "users_actions"("id");
 
 -- AddForeignKey
 ALTER TABLE "users_actions" ADD CONSTRAINT "users_actions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;

8b1dd9f9ae1aded55cd9c7b09cc13aa69298b216
Aleksandr <naberezhnyy.an@gmail.com>
2023-11-02 10:48:39 +0100

    devide index into app and server

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,23 +4,11 @@ import express from "express";
 import api from "./routes/api.router.js";
 import auth from "./routes/auth.router.js";
 
-const PORT = 8000;
-const app = express();
+export const app = express();
 
 app.use(cors())
 app.use(express.json());
 
-async function start() {
-  try {
-    app.listen(PORT, () =>
-      console.log("Server running and listen on port " + PORT)
-    );
-  } catch (err) {
-    console.log(err);
-  }
-}
-
 app.use("/auth", auth);
 app.use("/api", api);
 
-start();

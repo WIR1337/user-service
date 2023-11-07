2d7362b1c1d9ae3a765f21655580a04a438236e6
Aleksandr <naberezhnyy.an@gmail.com>
2023-11-07 10:28:14 +0100

    init socket.io

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
 import http from 'http';
+import { Server } from "socket.io";
 import { app } from "./app.js";
 const server = http.createServer(app)
+const io = new Server(server)
 
 const PORT = 8000;
 
 server.listen(PORT, () =>
   console.log("Server running and listen on port " + PORT)
 );
+
+io.on('connection', () => {
+    console.log('new connection')
+})

04aeaa48e4574e1095c8d8464375f914f7dce705
Aleksandr <naberezhnyy.an@gmail.com>
2023-11-11 14:25:53 +0100

    rebuild prisma schema

--- server/prisma/migrations/0_init/migration.sql
+++ server/prisma/migrations/0_init/migration.sql
@@ -1,14 +1,11 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('user', 'admin');
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
-    "role" "Role" NOT NULL DEFAULT 'user',
-    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" VARCHAR(20) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -16,19 +13,13 @@ CREATE TABLE "users" (
 -- CreateTable
 CREATE TABLE "users_actions" (
     "id" SERIAL NOT NULL,
-    "user_id" INTEGER NOT NULL,
+    "user_id" INTEGER,
     "action_type" VARCHAR(15) NOT NULL,
-    "action_data" JSONB NOT NULL,
-    "action_time" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "action_data" JSONB,
+    "action_time" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_actions_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_id_email_key" ON "users"("id", "email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_actions_id_key" ON "users_actions"("id");
 
 -- AddForeignKey
 ALTER TABLE "users_actions" ADD CONSTRAINT "users_actions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
